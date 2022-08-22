65911fc7ea9bf6e140af9a12e6e0822aab1a49c8
c.romero.hsv@gmail.com <c.romero.hsv@gmail.com>
2022-08-23 00:13:05 +0200

    dev corsMiddleware

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ require('dotenv').config();
 //const cookieParser = require('cookie-parser');
 const createError = require('http-errors');
 const express = require('express');
-//const logger = require('morgan');
+const logger = require('morgan');
 const path = require('path');
 const mongoose = require('mongoose');
 
@@ -20,7 +20,7 @@ const session = require('./config/session.config');
  */
 const app = express();
 app.use(cors);
-//app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser());

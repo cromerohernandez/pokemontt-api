1b527307151b4d414b9650b7867618ef5bc87ecf
CarlosRomeroHernandez <c.romero.hsv@gmail.com>
2022-09-01 20:15:47 +0200

    fix: allowedHeaders

--- config/cors.config.ts
+++ config/cors.config.ts
@@ -2,7 +2,7 @@ const cors = require('cors')
 
 const corsMiddleware = cors({
   origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-  allowedHeaders: ['Content-Type', 'Origin'],
+  allowedHeaders: ['Content-Type', 'origin'],
   credentials: true
 })
 

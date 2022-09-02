625a02c8139644b284ea8cf3c9eab6b0ccec51b0
CarlosRomeroHernandez <c.romero.hsv@gmail.com>
2022-09-02 22:47:53 +0200

    review function descriptions

--- middlewares/auth.middleware.ts
+++ middlewares/auth.middleware.ts
@@ -4,6 +4,11 @@ import { ISessionRequest } from '../interfaces/app.interfaces';
 
 const createError = require('http-errors');
 
+/**
+ * @description middleware function to check if user is authenticated
+ * @param req ISessionRequest
+ * @param next NextFunction
+ */
 module.exports.isAuthenticated = (req: ISessionRequest, res: Response, next: NextFunction) => {
   if (req.session.user) {
     next()
@@ -12,6 +17,11 @@ module.exports.isAuthenticated = (req: ISessionRequest, res: Response, next: Nex
   }
 };
 
+/**
+ * @description middleware function to check if user is not authenticated
+ * @param req ISessionRequest
+ * @param next NextFunction
+ */
 module.exports.isNotAuthenticated = (req: ISessionRequest, res: Response, next: NextFunction) => {
   if (req.session.user) {
     next(createError(403, 'USER_ALREADY_AUTHENTICATED'))

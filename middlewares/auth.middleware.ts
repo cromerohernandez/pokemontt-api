043e98c83e627d32a678427659d9f99c5ad983cd
CarlosRomeroHernandez <c.romero.hsv@gmail.com>
2022-09-03 18:44:57 +0200

    fixing req.session

--- middlewares/auth.middleware.ts
+++ middlewares/auth.middleware.ts
@@ -10,7 +10,7 @@ const createError = require('http-errors');
  * @param next NextFunction
  */
 module.exports.isAuthenticated = (req: ISessionRequest, res: Response, next: NextFunction) => {
-  console.log('CRH-req-isAuthenticated', req)
+  console.log('CRH-req-isAuthenticated', req.session)
 
   if (req.session.user) {
     next()

2f2e86e47f119b43eb28e2e1ea7518381f45a9e5
CarlosRomeroHernandez <c.romero.hsv@gmail.com>
2022-09-14 19:45:44 +0200

    deploy testing

--- config/cors.config.ts
+++ config/cors.config.ts
@@ -2,7 +2,9 @@ const cors = require('cors')
 
 const corsMiddleware = cors({
   origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-  allowedHeaders: ['Content-Type', origin],
+  allowedHeaders: ['Content-Type', 'origin'],
+  methods: ['GET', 'POST', 'PATCH'],
+  optionsSuccessStatus: 200,
   credentials: true,
 })
 

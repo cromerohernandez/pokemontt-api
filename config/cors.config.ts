20060a5fba6433b0fadfa75a1a8f893391299397
CarlosRomeroHernandez <c.romero.hsv@gmail.com>
2022-09-14 20:10:25 +0200

    deploy testing

--- config/cors.config.ts
+++ config/cors.config.ts
@@ -1,9 +1,9 @@
 const cors = require('cors')
 
 const corsMiddleware = cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  origin: 'https://pokemontt-arena.netlify.app', //process.env.CORS_ORIGIN || 'http://localhost:3000',
   allowedHeaders: ['Content-Type', 'origin'],
-  headers: {'Access-Control-Allow-Origin': 'https://pokemontt-arena.netlify.app'},
+  headers: {'Access-Control-Allow-Origin': 'https://pokemontt-api.netlify.app'},
   methods: ['GET', 'POST', 'PATCH'],
   optionsSuccessStatus: 200,
   credentials: true,

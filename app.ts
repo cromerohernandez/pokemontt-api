c82692f8188f21882c8a3ab054f8690d5f3d21b1
CarlosRomeroHernandez <c.romero.hsv@gmail.com>
2022-09-17 20:06:04 +0200

    deployment test

--- app.ts
+++ app.ts
@@ -21,7 +21,7 @@ const cors = require('./config/cors.config');
  */
 const app = express();
 app.use(cors);
-app.use(logger('dev'));
+app.use(logger());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());

0de5d928dbf583d7c0d8c528416746193d17b619
CarlosRomeroHernandez <c.romero.hsv@gmail.com>
2022-09-18 22:59:47 +0200

    set session to ios

--- config/cors.config.ts
+++ config/cors.config.ts
@@ -2,7 +2,7 @@ const cors = require('cors')
 
 const corsMiddleware = cors({
   origin: process.env.CORS_ORIGIN,
-  allowedHeaders: ['Accept', 'Accept-Encoding', 'Accept-Language', 'Access-Control-Allow-Headers', 'Authorization', 'Content-Type', 'X-Requested-With', 'Origin'],
+  allowedHeaders: ['Accept','Accept-Encoding','Accept-Language','Access-Control-Request-Header','Access-Control-Request-Method','Authorization','Cache-Control','Connection','Content-Type','DNT','Host','If-Modified-Since','Keep-Alive','Origin','Pragma','Referer','User-Agent','x-csrf-token','x-requested-with', 'Accept', 'Accept-Encoding', 'Accept-Language', 'Access-Control-Allow-Headers', 'Authorization', 'Content-Type', 'X-Requested-With', 'Origin'],
   credentials: true,
 })
 
